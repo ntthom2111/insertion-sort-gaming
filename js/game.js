ad39e17dcd4bbec4e0707191ff33ea917de38425
ntthom2111 <155417560+ntthom2111@users.noreply.github.com>
2024-01-10 10:11:53 +0700

    c1

--- js/game.js
+++ js/game.js
@@ -5,8 +5,8 @@ import Mainscene from "./scene.js"
 
 const config ={
     type: Phaser.AUTO,
-    width: 1920,
-    height:1080,
+    width:1468,
+    height:768,
     physics: {
         default: "arcade",
       },
