a9a99d6243fa72f15a72aa8c082d3df57ead7d27
ntthom2111 <155417560+ntthom2111@users.noreply.github.com>
2024-01-11 09:14:36 +0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -3,18 +3,19 @@ import Mainscene from "./scene.js"
 
 //import Main2 from "./main2.js";
 
-const config ={
-    type: Phaser.AUTO,
-    width:1468,
-    height:768,
-    physics: {
-        default: "arcade",
-      },
-    backgroundColor:0x1B1B1D,
-    scene: [Mainscene]
+const config = {
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER
+},	
+ screen: this
+}
 
   
-}
+
 
 
 
