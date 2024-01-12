2534261c323ddd3171d0ec1acf0201d6f3de0716
ntthom2111 <155417560+ntthom2111@users.noreply.github.com>
2024-01-13 00:44:24 +0700

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -23,7 +23,7 @@ export default class Mainscene extends Phaser.Scene{
       this.load.audio('game', '../assets/music/game.mp3');
   }
   create(){
-     const Theme = this.add.image(1468/2, 768/2, 'theme')
+     const Theme = this.add.image( 'theme')
      Theme.setScale(0.95)
      this.sound.play('game', { volume:  0.1
       
